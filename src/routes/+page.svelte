c99966e3d7d54b5fcff8c85e9377b3b7b6972580
potatojoayo <potatojoayo@gmail.com>
2023-08-10 23:01:16 +0900

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -73,7 +73,7 @@
     </div>
 
     <h2 class="text-2xl">Hobby</h2>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/8bcgRtUCdFg" title="YouTube video player"
+    <iframe src="https://www.youtube.com/embed/8bcgRtUCdFg" title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>
